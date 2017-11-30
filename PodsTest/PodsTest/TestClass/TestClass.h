d948de466d013fc349fcbfa27d58df46e5a6b30b
qiuchengxiang@gmail.com <qiuchengxiang@zillionfortune.com>
2017-11-30 15:30:32 +0800

    1111

---
+++ PodsTest/PodsTest/TestClass/TestClass.h
@@ -0,0 +1,13 @@
+//
+//  TestClass.h
+//  PodsTest
+//
+//  Created by qiuchengxiang@gmail.com on 2017/11/30.
+//  Copyright © 2017年 Zillion Fortune. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
---
+++ PodsTest/PodsTest/TestClass/TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  PodsTest
+//
+//  Created by qiuchengxiang@gmail.com on 2017/11/30.
+//  Copyright © 2017年 Zillion Fortune. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
